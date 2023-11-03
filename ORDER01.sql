20cee3f6b675d6a7023fc1634529b95129a7aa50
Karol <93782725+petitess@users.noreply.github.com>
2023-11-03 16:45:43 +0100

    order descending

---
+++ ORDER01.sql
@@ -0,0 +1,12 @@
+SELECT * FROM TutorialAppSchema.Computer
+SELECT * FROM TutorialAppSchema.Computer WHERE Motherboard = 'Voonte'
+SELECT * FROM TutorialAppSchema.Computer WHERE ReleaseDate < '2020-01-01'
+SELECT [ComputerId]
+[Motherboard],
+ISNULL([CPUCores], 4) AS CPUCores,
+[HasWifi],
+[HasLTE],
+[ReleaseDate],
+[Price],
+[VideoCard] FROM TutorialAppSchema.Computer
+ORDER BY ReleaseDate DESC
