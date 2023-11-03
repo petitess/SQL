02cdc0320eb301df1495c33d846d6799e037cd96
Karol <93782725+petitess@users.noreply.github.com>
2023-11-03 16:47:07 +0100

    insert data

---
+++ INSERT01.sql
@@ -0,0 +1,20 @@
+INSERT INTO TutorialAppSchema.Computer
+    (
+    [Motherboard],
+    [CPUCores],
+    [HasWifi],
+    [HasLTE],
+    [ReleaseDate],
+    [Price],
+    [VideoCard]
+    )
+VALUES
+    (
+    'Sample-Motherboard',
+    4,
+    2,
+    0,
+    '2022-01-01',
+    1000,
+    'NVIDIA'
+    )
