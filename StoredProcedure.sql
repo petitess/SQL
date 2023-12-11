8f1a090fc539838bced8005838b54f43eb542ebe
Karol <93782725+petitess@users.noreply.github.com>
2023-12-11 07:15:50 +0100

    Update StoredProcedure.sql

--- StoredProcedure.sql
+++ StoredProcedure.sql
@@ -17,3 +17,28 @@ BEGIN
         WHERE Users.UserId = ISNULL(@UserId, Users.UserId)
 END
 ------
+USE DotNetCourseDatabase;
+GO
+
+ALTER PROCEDURE TutorialAppSchema.spUsers_Get 
+/*EXEC TutorialAppSchema.spUsers_Get @UserId=3*/
+--Parameter
+    @UserId INT = NULL
+AS 
+BEGIN
+    SELECT [Users].[UserId],
+    [Users].[FirstName],
+    [Users].[LastName],
+    [Users].[Email],
+    [Users].[Gender],
+    [Users].[Active],
+    UserSalary.Salary,
+    UserJobInfo.JobTitle,
+    UserJobInfo.Department
+    From TutorialAppSchema.Users AS Users
+        LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary
+            ON UserSalary.UserId = Users.UserId
+        LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+            ON UserJobInfo.UserId = Users.UserId
+        WHERE Users.UserId = ISNULL(@UserId, Users.UserId)
+END
