4e5795d910250eb616860a7e9052b484ad654c4e
Karol <93782725+petitess@users.noreply.github.com>
2023-12-11 17:38:58 +0100

    Update StoredProcedure.sql

--- StoredProcedure.sql
+++ StoredProcedure.sql
@@ -52,15 +52,6 @@ ALTER PROCEDURE TutorialAppSchema.spUsers_Get
     @UserId INT = NULL
 AS 
 BEGIN
-    -- SELECT UserJobInfo.Department
-    -- , AVG(UserSalary.Salary) AvgSalary
-    -- From TutorialAppSchema.Users AS Users
-    --     LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary
-    --     ON UserSalary.UserId = Users.UserId
-    --     LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
-    --     ON UserJobInfo.UserId = Users.UserId
-    -- GROUP BY UserJobInfo.Department
-
     SELECT [Users].[UserId],
         [Users].[FirstName],
         [Users].[LastName],
@@ -84,7 +75,57 @@ BEGIN
             LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo2
             ON UserJobInfo2.UserId = Users.UserId
         WHERE UserJobInfo2.Department = UserJobInfo.Department
-        GROUP BY UserJobInfo2.Department) AvgSalary
+        GROUP BY UserJobInfo2.Department) AS AvgSalary
     WHERE Users.UserId = ISNULL(@UserId, Users.UserId)
 END
 ----------
+USE DotNetCourseDatabase;
+GO
+
+ALTER PROCEDURE TutorialAppSchema.spUsers_Get
+    /*EXEC TutorialAppSchema.spUsers_Get @UserId=3*/
+    --Parameter
+    @UserId INT = NULL
+    , @Active BIT  = NULL
+
+AS 
+BEGIN
+    --FOR OLD DATABASE VERSION
+    IF OBJECT_ID('tempdb..#AverageDeptSalary', 'U') IS NOT NULL
+        BEGIN
+            DROP TABLE #AverageDeptSalary
+        END
+    --FOR NEW DATABASES
+    --DROP TABLE IF EXISTS #AverageDeptSalary 
+    SELECT UserJobInfo.Department
+        , AVG(UserSalary.Salary) AvgSalary
+        INTO #AverageDeptSalary
+    From TutorialAppSchema.Users AS Users
+        LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary
+        ON UserSalary.UserId = Users.UserId
+        LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+        ON UserJobInfo.UserId = Users.UserId
+    GROUP BY UserJobInfo.Department
+
+    CREATE CLUSTERED INDEX cix_AverageDeptSalary_Department ON #AverageDeptSalary(Department)
+
+    SELECT [Users].[UserId],
+        [Users].[FirstName],
+        [Users].[LastName],
+        [Users].[Email],
+        [Users].[Gender],
+        [Users].[Active],
+        UserSalary.Salary,
+        UserJobInfo.JobTitle,
+        UserJobInfo.Department,
+        AvgSalary.AvgSalary
+    From TutorialAppSchema.Users AS Users
+        LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary
+        ON UserSalary.UserId = Users.UserId
+        LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+        ON UserJobInfo.UserId = Users.UserId
+    LEFT JOIN #AverageDeptSalary AS AvgSalary
+        ON AvgSalary.Department = UserJobInfo.Department
+    WHERE Users.UserId = ISNULL(@UserId, Users.UserId)
+        AND ISNULL(Users.Active, 0) = COALESCE(@Active, Users.Active, 0)
+END
