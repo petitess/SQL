e96111491f7fa2ee4ce41c6f213087a2d80c64c1
Karol <93782725+petitess@users.noreply.github.com>
2023-12-12 17:13:04 +0100

    Update StoredProcedure.sql

--- StoredProcedure.sql
+++ StoredProcedure.sql
@@ -250,3 +250,46 @@ BEGIN
         OR PostTitle LIKE '%' + @SearchValue + '%')
 END
 ------------------------
+
+USE DotNetCourseDatabase;
+GO
+
+CREATE OR ALTER PROCEDURE TutorialAppSchema.spPost_Upsert
+
+    @UserId INT,
+    @PostTitle NVARCHAR(255),
+    @PostContent NVARCHAR(MAX),
+    @PostId INT = NULL
+AS
+BEGIN
+    IF NOT EXISTS ( SELECT *
+    FROM TutorialAppSchema.Posts
+    WHERE PostId = @PostId)
+            BEGIN
+
+        INSERT INTO TutorialAppSchema.Posts
+            ([UserId],
+            [PostTitle],
+            [PostContent],
+            [PostCreated],
+            [PostUpdated]
+            )
+        VALUES
+            (
+                @UserId,
+                @PostTitle,
+                @PostContent,
+                GETDATE(),
+                GETDATE()
+            )
+    END
+        ELSE
+            BEGIN
+        UPDATE TutorialAppSchema.Posts
+                    SET PostTitle = @PostTitle,
+                        PostContent = @PostContent,
+                        PostUpdated = GETDATE()
+                    WHERE PostId = @PostId
+    END
+END
+--------------------
