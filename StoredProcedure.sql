f15529696682bad967e814d8cab10de7932072b8
Karol <93782725+petitess@users.noreply.github.com>
2023-12-12 17:17:27 +0100

    Update StoredProcedure.sql

--- StoredProcedure.sql
+++ StoredProcedure.sql
@@ -293,3 +293,16 @@ BEGIN
     END
 END
 --------------------
+USE DotNetCourseDatabase;
+GO
+
+CREATE OR ALTER PROCEDURE TutorialAppSchema.spPost_Delete
+    @PostId INT,
+    @UserId INT
+AS
+
+BEGIN
+    DELETE FROM TutorialAppSchema.Posts WHERE PostId = @PostId
+        AND UserId = @UserId
+END
+-------------------
