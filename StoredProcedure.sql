d7af7abc1f5bdcc74423ff15517235dd31f54cde
Karol <93782725+petitess@users.noreply.github.com>
2023-12-12 16:37:52 +0100

    Update StoredProcedure.sql

--- StoredProcedure.sql
+++ StoredProcedure.sql
@@ -213,3 +213,15 @@ BEGIN
     END
 END
 ------------
+USE DotNetCourseDatabase;
+GO
+
+CREATE PROCEDURE TutorialAppSchema.spUser_Delete
+    @UserId INT
+AS
+BEGIN
+    DELETE FROM TutorialAppSchema.Users WHERE UserId = @UserId
+    DELETE FROM TutorialAppSchema.UserSalary WHERE UserId = @UserId
+    DELETE FROM TutorialAppSchema.UserJobInfo WHERE UserId = @UserId
+END
+---------------
