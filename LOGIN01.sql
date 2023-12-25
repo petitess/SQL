daefc074c6839eaf9bccbc7c713fb877091ac50e
Karol <93782725+petitess@users.noreply.github.com>
2023-12-25 09:54:41 +0100

    Update LOGIN01.sql

--- LOGIN01.sql
+++ LOGIN01.sql
@@ -7,3 +7,7 @@ EXEC sp_addrolemember 'db_datareader', 'DESKTOP-CFCPFSG\JOBB';
 --Grant owner permission
 use DotNetCourseDatabase
 exec sp_addrolemember 'db_owner', 'DESKTOP-CFCPFSG\JOBB';
+--Add managed identity
+CREATE USER [func-weather-prod-01] FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER [func-weather-prod-01]
+ALTER ROLE db_datawriter ADD MEMBER [func-weather-prod-01]
